a14d1a1b62198f5c5be5ac27cf6b394f4e4530cc
Silvia <s.tamburini1@campus.unimib.it>
2022-03-18 19:57:37 +0100

    added function for data anonimization
    Aggiunta funzione che anonimizza le coordinate dei punti, così da poter mostrare nella tesi delle immagini senza mostrare dati sensibili.

--- DataImport.py
+++ DataImport.py
@@ -1,8 +1,10 @@
+
 #************************************************************************************
 # Questo file contiene le funzioni necessarie a importare e modificare i dati
 #************************************************************************************
 
 import pandas as pd
+import numpy as np
 
 #----------------------------------
 # Funzione di data preparation
@@ -13,7 +15,10 @@ def db_preparation(figures):
     """Standardizzazione del database
     
     Il database di input è un database già importato in Pandas che contiene, tra le varie colonne, 
-    la colonna ID_EXT e la colonna GEOM, dove la colonna ID_EXT contiene l'id del poligono, e la colonna GEOM è l'elenco delle coordinate: in particolare, contiene prima alcune tag di GML, e poi le coordinate separate da virgola, e infine le end tag di GML. Questo codice vuole creare un db lungo che contiene la Latitudine e la Longitudine in colonne distinte.
+    la colonna ID_EXT e la colonna GEOM, dove la colonna ID_EXT contiene l'id del poligono, e la colonna GEOM 
+    è l'elenco delle coordinate: in particolare, contiene prima alcune tag di GML, e poi le coordinate 
+    separate da virgola, e infine le end tag di GML. Questo codice vuole creare un db lungo che contiene 
+    la Latitudine e la Longitudine in colonne distinte.
     """
 
     #TODO: check campi
@@ -41,3 +46,30 @@ def db_preparation(figures):
     figures = figures.drop(labels = ['GEOM', 'GEOM_corr', 'coords'], axis = 1)
     
     return figures
+
+
+#--------------------------------------
+# Funzione di anonimizzazione dei dati
+#--------------------------------------
+
+def anonimization(figura):
+
+    """Funzione di anonimizzazione dei punti
+    
+    Funzione di anonimizzazione dei punti dati per poter riportare nella tesi alcune immagini relative ai poligoni senza rivelare le coordinate degli impianti effettivi.Questa funzione anonimizza tutto ponendo un punto qualunque a (0,0) e calcolando latitudine e longitudine relativa rispetto a quel punto; e poi aggiunge un rumore bianco gaussiano con media 0 e deviazione standard 1e-5 (per non spostare troppo i punti). 
+    """
+
+    # Punto di riferimento
+    latref = figura['Latitude'].iloc[0]
+    lonref = figura['Longitude'].iloc[0]
+
+    # Anonimizzazione relativa al primo punto
+    figura['Latitude'] = figura['Latitude']-latref
+    figura['Longitude'] = figura['Longitude']-lonref
+
+    # Rumore bianco
+    noise = np.random.normal(0,1e-5,len(figura))
+    figura['Latitude'] = figura['Latitude'] + noise
+    figura['Longitude'] = figura['Longitude'] + noise
+
+    return figura
